ef7cff042624c92c9a1eb863ea7575208be73f6c
Joe Van Dyk <joe@fixieconsulting.com>
2012-04-25 15:28:05 -0700

    wip

--- monkey_slugs.rb
+++ monkey_slugs.rb
@@ -2,51 +2,74 @@ module MonkeySlugs
   ROUTE = { :id => /.*/ }
 
   def self.sluggify klass, options={}
+    options = {
+      :use_uuid             => true,
+      :friendly_name_source => nil,
+      :slug_column          => :uuid,
+      :update_slug_on_save  => false
+    }.merge!(options)
+
     klass.send :include, MonkeySlugs::Slug
+
+    klass.use_uuid             = options[:use_uuid]
+    klass.friendly_name_source = options[:friendly_name_source]
+    klass.slug_column          = options[:slug_column]
   end
 
   module Slug
     extend ActiveSupport::Concern
 
     included do
-      before_create   :set_uuid_column
-      class_attribute :uuid_column_name
-      class_attribute :has_friendly_name
-
-      self.uuid_column_name = :uuid
-      self.has_friendly_name = true
-    end
-
-    def set_uuid_column
-      self.uuid_column ||= SecureRandom.hex(5)
+      class_attribute :use_uuid
+      class_attribute :friendly_name_source
+      class_attribute :slug_column
+      before_save :update_slug_column
     end
 
     def to_param
-      if self.class.has_friendly_name?
-        "#{friendly_name}/#{uuid_column}"
+      if self.class.use_uuid?
+        "#{friendly_name}/#{slug_value}"
       else
-        send self.class.uuid_column_name
+        slug_value
       end
     end
 
-    def uuid_column
-      send self.class.uuid_column_name
+    def update_slug_column
+      if use_uuid? and slug_value.blank?
+        self.slug_value = generate_uuid
+      else
+        f = friendly_name
+        if slug_value != f
+          self.slug_value = f
+        end
+      end
     end
 
-    def uuid_column= value
-      write_attribute self.class.uuid_column_name, value
+    def generate_uuid
+      SecureRandom.hex(5)
+    end
+
+    def slug_value
+      send(slug_column) || send(self.class.primary_key)
+    end
+
+    def slug_value= value
+      write_attribute self.slug_column, value
     end
 
     def correct_friendly_name? id
-      if self.class.has_friendly_name?
+      if use_uuid?
         self.class.extract_friendly_name(id) == friendly_name
       else
-        uuid_column == id
+        slug_value == id
       end
     end
 
-
     def friendly_name
+      if friendly_name_source
+        return send(friendly_name_source).to_s
+      end
+
       if respond_to?(:name)
         name
       elsif respond_to?(:title)
@@ -59,37 +82,25 @@ module MonkeySlugs
     module ClassMethods
       # There's probably a better way to do this.
       def extract_uuid id
-        if has_friendly_name?
+        if use_uuid?
           id.split('/').last
         else
           id
         end
       end
 
-      def has_friendly_name?
-        has_friendly_name.inspect
-      end
-
       def extract_friendly_name id
-        if has_friendly_name?
+        if use_uuid?
           id.split('/')[0..-2].join('/')
         else
           id
         end
       end
 
-      def has_uuid? id
-        !has_friendly_name? || id.include?('/')
-      end
-
       # What's the correct way to do this?
       def find id, *args, &block
-        if has_uuid?(id)
-          uuid = extract_uuid(id)
-          where(uuid_column_name => uuid).first || super
-        else
-          super
-        end
+        uuid = extract_uuid(id)
+        where("#{slug_column} = ? or #{primary_key} = ?", uuid, uuid).first || super
       end
     end
   end
