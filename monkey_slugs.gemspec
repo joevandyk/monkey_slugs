bc823772bd79b16780a43f3e2c3850750d8faf1c
Joe Van Dyk <joe@fixieconsulting.com>
2012-04-25 12:10:19 -0700

    first version of monkey_slugs

---
+++ monkey_slugs.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+  s.name = %q{monkey_slugs}
+  s.version = "0.0.1"
+  s.date = %q{2011-09-29}
+  s.summary = %q{monkey slugs is great}
+  s.files = [
+    "monkey_slugs.rb"
+  ]
+  s.require_paths = ["."]
+end
---
+++ monkey_slugs.rb
@@ -0,0 +1,52 @@
+module MonkeySlugs
+  ROUTE = { :id => /.*/ }
+
+  def self.sluggify klass, options={}
+    klass.send :include, MonkeySlugs::Slug
+    klass.send :define_method, :friendly_name, options[:how]
+  end
+
+  module Slug
+    extend ActiveSupport::Concern
+
+    included do
+      before_create :set_uuid
+    end
+
+    def set_uuid
+      self.uuid ||= SecureRandom.hex(4)
+    end
+
+    def to_param
+      "#{friendly_name}/#{uuid}"
+    end
+
+    def correct_slug? id
+      self.class.extract_friendly_name(id) == friendly_name
+    end
+
+    module ClassMethods
+      # There's probably a better way to do this.
+      def extract_uuid id
+        id.split('/').last
+      end
+
+      def extract_friendly_name id
+        id.split('/').first
+      end
+
+      def has_uuid? id
+        id.include?('/')
+      end
+
+      def find id, *args, &block
+        if has_uuid?(id)
+          uuid = extract_uuid(id)
+          where(:uuid => uuid).first || super
+        else
+          super
+        end
+      end
+    end
+  end
+end
